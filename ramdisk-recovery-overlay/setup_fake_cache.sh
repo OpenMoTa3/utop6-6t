593e99bf0c49ec75d32cae6db00a53f4b4870baf
Rúben Carneiro <rubencarneiro01@gmail.com>
2026-03-01 22:05:36 +0000

    Update device tree

--- ramdisk-recovery-overlay/setup_fake_cache.sh
+++ ramdisk-recovery-overlay/setup_fake_cache.sh
@@ -1,26 +1,16 @@
-#!/system/bin/sh
+#!/sbin/sh
 
 DATA_MOUNT_CODE=1
 
-RETRY_COUNTER=0
 while [ "$DATA_MOUNT_CODE" != "0" ]; do
-    if [ "$RETRY_COUNTER" -gt "10" ]; then
-        echo "setup_fake_cache.sh - reached maximum number of retries" > /dev/kmsg
-
-        setprop halium.datamount.done 1
-        exit 1
-    fi
-    RETRY_COUNTER=$((RETRY_COUNTER+1))
-
-    mount /dev/block/bootdevice/by-name/userdata /data > /dev/kmsg
+    mount -t ext4 /dev/block/bootdevice/by-name/userdata /data > /dev/kmsg
     DATA_MOUNT_CODE=$?
     sleep 1
 done
 
-if [ "$DATA_MOUNT_CODE" == "0" ]; then
-    mkdir /data/cache > /dev/kmsg
-    mount -o bind /data/cache /cache > /dev/kmsg
-fi
+mkdir /data/cache > /dev/kmsg
+mount -o bind /data/cache /cache > /dev/kmsgs
 
 setprop halium.datamount.done 1
+
 exit 0
